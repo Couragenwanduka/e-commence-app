4c88b3801f40ac4d7abd51b29a1961d45a00a296
Couragenwanduka <156565155+Couragenwanduka@users.noreply.github.com>
2024-01-18 08:56:57 +0100

    Update e-commence.js

--- e-commence.js
+++ e-commence.js
@@ -3,14 +3,14 @@
 
  let store="These are the few items we have \r\r 1.brush #1000 \r 2.toothpaste #500\r 3.shirts #3000\r 4. singlet #1500\r 5.boxers #2000\r\r\r please Input a number";
   let first= prompt(store);
-  if(first !== undefined){
-    alert("You didn't make any input")
+  if(first == undefined){
+    alert("You didn't make any input ")
   }else{alert("Thanks for shopping with us")
   }
   
   let quantity= "How many of the product do you want";
   let second=prompt(quantity);
-if(second !== undefined){
+if(second == undefined){
   alert("You didn't make any input")
 }else{alert("Thanks for shopping with us")
 }
